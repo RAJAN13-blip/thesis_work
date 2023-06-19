6a95e7b0537e7db94bb059d05269862b1b53eebf
RAJAN13-blip <f20190572@pilani.bits-pilani.ac.in>
2023-06-19 18:14:17 +0530

    Day -2 minimalist arch done

--- minimalist_diff_model.py
+++ minimalist_diff_model.py
@@ -8,15 +8,36 @@ class DiffusionNet(nn.Module):
     def __init__(self, channels=[32,64,128,256]):
         super().__init__()
 
+        #convolutional blocks --> down resolution 
         self.conv1 = nn.Conv2d(1          ,channels[0],kernel_size=3,stride=1,bias=False)
         self.conv2 = nn.Conv2d(channels[0],channels[1],kernel_size=3, stride=2,bias=False)
         self.conv3 = nn.Conv2d(channels[1],channels[2],kernel_size=3,stride=2,bias=False)
         self.conv4 = nn.Conv2d(channels[2],channels[3],kernel_size=3,stride=2,bias=False)
 
-
-        self.upconv1 = nn.ConvTranspose2d(channels[3],channels[2],kernel_size=3, stride=2, bias=False)
-        self.upconv2 = nn.ConvTranspose2d(channels[2],channels[1],kernel_size=3, stride=2, bias=False)
-        self.upconv3 = nn.ConvTranspose2d(channels[1],channels[0],kernel_size=3, stride=2, bias=False) 
-        self.upconv4 = nn.ConvTranspose2d(channels[0],1         , kernel_size=3, stride=1, bias=False)
+        #convolutional blocks --> up resolution (residual blocks)
+        self.upconv1 = nn.ConvTranspose2d(channels[3]            ,channels[2],kernel_size=3, stride=2, bias=False) 
+        self.upconv2 = nn.ConvTranspose2d(channels[2]+channels[2],channels[1],kernel_size=3, stride=2, bias=False)
+        self.upconv3 = nn.ConvTranspose2d(channels[1]+channels[1],channels[0],kernel_size=3, stride=2, bias=False) 
+        self.upconv4 = nn.ConvTranspose2d(channels[0]+channels[0],1         , kernel_size=3, stride=1, bias=False)
 
         
+        #group-norm layers for downsampling 
+        self.gnorm1 = nn.GroupNorm( 4, channels[0])
+        self.gnorm2 = nn.GroupNorm(32, channels[1])
+        self.gnorm3 = nn.GroupNorm(32, channels[2])
+        self.gnorm4 = nn.GroupNorm(32, channels[3])
+
+        #group-norm layers for upsampling
+        self.gnorm5 = nn.GroupNorm( 32, channels[2]) 
+        self.gnorm6 = nn.GroupNorm( 32, channels[1]) 
+        self.gnorm7 = nn.GroupNorm( 32, channels[0]) 
+
+        #activation layers 
+        self.act_relu = lambda x : F.relu(x)
+        self.act_swish = lambda x : x * torch.sigmoid(x)
+        
+    def forward(self,x):
+        pass
+
+
+
