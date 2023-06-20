d0f729b1f6daf3aa8f5d3cfbc6f8a29dfadfd8ed
RAJAN13-blip <f20190572@pilani.bits-pilani.ac.in>
2023-06-20 13:23:47 +0530

    Day 3 fininshed implementing minimal diff

--- minimalist_diff_model.py
+++ minimalist_diff_model.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 import numpy as np
 
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+#Architecture of the model -- MNIST use case
 class DiffusionNet(nn.Module):
     def __init__(self, channels=[32,64,128,256]):
         super().__init__()
@@ -16,8 +19,8 @@ class DiffusionNet(nn.Module):
 
         #convolutional blocks --> up resolution (residual blocks)
         self.upconv1 = nn.ConvTranspose2d(channels[3]            ,channels[2],kernel_size=3, stride=2, bias=False) 
-        self.upconv2 = nn.ConvTranspose2d(channels[2]+channels[2],channels[1],kernel_size=3, stride=2, bias=False)
-        self.upconv3 = nn.ConvTranspose2d(channels[1]+channels[1],channels[0],kernel_size=3, stride=2, bias=False) 
+        self.upconv2 = nn.ConvTranspose2d(channels[2]+channels[2],channels[1],kernel_size=3, stride=2, bias=False,output_padding=1)
+        self.upconv3 = nn.ConvTranspose2d(channels[1]+channels[1],channels[0],kernel_size=3, stride=2, bias=False,output_padding=1) 
         self.upconv4 = nn.ConvTranspose2d(channels[0]+channels[0],1         , kernel_size=3, stride=1, bias=False)
 
         
@@ -37,7 +40,40 @@ class DiffusionNet(nn.Module):
         self.act_swish = lambda x : x * torch.sigmoid(x)
         
     def forward(self,x):
-        pass
+        
+        #downsampling
+        h1 = self.conv1(x)
+        h1 = self.gnorm1(h1)
+        h1 = self.act_relu(h1)
 
+        h2 = self.conv2(h1)
+        h2 = self.gnorm2(h2)
+        h2 = self.act_relu(h2)
 
+        h3 = self.conv3(h2)
+        h3 = self.gnorm3(h3)
+        h3 = self.act_relu(h3)
+
+        h4 = self.conv4(h3)
+        h4 = self.gnorm4(h4)
+        h4 = self.act_relu(h4)
+
+        #upsampling
+        h = self.upconv1(h4)
+        h = self.gnorm5(h)
+        h = self.act_relu(h)
+
+        h = self.upconv2(torch.cat([h, h3],dim=1))
+        h = self.gnorm6(h)
+        h = self.act_relu(h)
+
+        h = self.upconv3(torch.cat([h,h2],dim=1))
+        h = self.gnorm7(h)
+        h = self.act_relu(h)
+
+        h = self.upconv4(torch.cat([h,h1],dim=1))
+        h = self.act_swish(h)
+    
+        return h
+    
 
--- minimalist_diffusion.py
+++ minimalist_diffusion.py
@@ -9,5 +9,65 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import tqdm 
 
+from minimalist_diff_model import DiffusionNet,device
+
+#Setting up the parameters
+batch_size = 32
+num_epochs = 50
+learning_rate = 1e-4
+model = torch.nn.DataParallel(DiffusionNet())
+model = model.to(device=device)
+datasets = MNIST('.',train=True,transform=transforms.ToTensor(),download=True)
+data_loader = DataLoader(dataset=datasets,batch_size=batch_size,shuffle=True,num_workers=4)
+optimizer = Adam(model.parameters(),lr=learning_rate)
+time_steps = 100
 
 
+#define an alpha schedule 
+def alpha_linear_schedule(steps):
+    """steps : number of steps
+    """
+    return torch.linspace(0,1,steps)
+
+#defining the loss function
+def loss_fn(model:DiffusionNet,
+            x1:torch.tensor):
+    """model : the minimialistic diffusion model
+          x1 : tensor for manipulating data (alpha deblending)
+    """
+    x0 = torch.randn_like(x1)
+    alpha = torch.randn(1,).uniform_(0,1)
+    
+    blended_x = (1. - alpha)*x0 + (alpha)*x1
+    model_output = model(blended_x)
+    loss = torch.mean(torch.square(model_output - (x1-x0)))
+    return loss
+
+
+#Training loop 
+for epoch in range(num_epochs):
+    loss = 0
+    num_items = 0
+    for x, y in data_loader:
+        x = x.to(device)
+        y = y.to(device)
+        loss = loss_fn(model,x)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        num_items += x.shape[0]
+    print('Average Loss: {:5f}'.format(loss / num_items))
+  # Update the checkpoint after each epoch of training.
+    torch.save(model.state_dict(), 'ckpt.pth')
+
+
+#for sampling procedure
+alpha_schedule = alpha_linear_schedule(time_steps)
+
+
+#Sampling steps 
+def sampler():
+    pass
+
+def improved_sampler():
+    pass
